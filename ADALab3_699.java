e8622b13762d5b3bca279efac467b17da53045ee
JARVIdioticS <114250375+JARVIdioticS@users.noreply.github.com>
2023-01-30 14:39:38 +0700

    Update ADALab3_699.java

--- ADALab3_699.java
+++ ADALab3_699.java
@@ -6,8 +6,25 @@ public class ADALab3_699 {
         Random rd = new Random();
         Scanner sn = new Scanner(System.in);
 
+        //fill inputs
         int count = 0;
-        for (int i = 0; i < arr.length; i++){
-            arr[i] = rd.nextInt(100);
+        for (int i = 0; i < arr.length; i++) arr[i] = rd.nextInt(100);
+        
+        while (isSorted(arr) == false) shuffle(arr);
+    }
+    
+    private boolean isSorted(int[]arr)
+    {
+        for (int i = 1; i < a.length; i++)
+            if (a[i] < a[i - 1]) return false;
+        return true;
+    }
+
+    private void shuffle(int[]arr)
+    {
+		List<Integer> intList = Arrays.asList(arr);
+		Collections.shuffle(intList);
+		intList.toArray(arr);
+		System.out.println(Arrays.toString(arr));
     }
 }
