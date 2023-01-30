5065cac72c0629a2df4857ae7b4a9093a8f7ac45
JARVIdioticS <114250375+JARVIdioticS@users.noreply.github.com>
2023-01-30 14:51:01 +0700

    Update ADALab3_699.java

--- ADALab3_699.java
+++ ADALab3_699.java
@@ -2,29 +2,32 @@
 import java.util.*;
 public class ADALab3_699 {
     public static void main(String args[]) {
-        int arr[] = new int[100];
+        Integer arr[] = new Integer[100];
         Random rd = new Random();
-        Scanner sn = new Scanner(System.in);
 
         //fill inputs
         int count = 0;
         for (int i = 0; i < arr.length; i++) arr[i] = rd.nextInt(100);
         
-        while (isSorted(arr) == false) shuffle(arr);
+        while (isSorted(arr) == false) {
+		count++;
+		shuffle(arr);
+	}
+	System.out.println("Count: "+ count);
     }
     
-    private boolean isSorted(int[]arr)
+    private static boolean isSorted(Integer[]arr)
     {
-        for (int i = 1; i < a.length; i++)
-            if (a[i] < a[i - 1]) return false;
+        for (int i = 1; i < arr.length; i++)
+            if (arr[i] < arr[i - 1]) return false;
         return true;
     }
 
-    private void shuffle(int[]arr)
+    private static void shuffle(Integer[]arr)
     {
-		List<Integer> intList = Arrays.asList(arr);
-		Collections.shuffle(intList);
-		intList.toArray(arr);
-		System.out.println(Arrays.toString(arr));
+	List<Integer> intList = Arrays.asList(arr);
+	Collections.shuffle(intList);
+	intList.toArray(arr);
+	System.out.println(Arrays.toString(arr));
     }
 }
