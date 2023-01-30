7556153fa5850c0dc934b9a7580151fb8c8746cb
JARVIdioticS <114250375+JARVIdioticS@users.noreply.github.com>
2023-01-30 14:20:19 +0700

    Update and rename ADALab2_699.java to ADALab3_699.java

---
+++ ADALab3_699.java
@@ -0,0 +1,13 @@
+//64050699 สรวิศ วงศ์ษา
+import java.util.*;
+public class ADALab3_699 {
+    public static void main(String args[]) {
+        int arr[] = new int[100];
+        Random rd = new Random();
+        Scanner sn = new Scanner(System.in);
+
+        int count = 0;
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = rd.nextInt(100);
+    }
+}
